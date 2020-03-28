4a3145abfc74977b61b6f6efb64b8a63e04aed43
Krzem <50489938+Krzem5@users.noreply.github.com>
2020-03-28 15:55:58 +0100

    Push Update

---
+++ com/krzem/mario/LevelBlockData.java
@@ -0,0 +1,24 @@
+package com.krzem.mario;
+
+
+
+
+
+
+public class LevelBlockData extends Constants{
+	public int id;
+	public int x;
+	public int y;
+	public int dl;
+	public byte[] d;
+
+
+
+	public LevelBlockData(int id,int x,int y,int dl,byte[] d){
+		this.id=id;
+		this.x=x;
+		this.y=y;
+		this.dl=dl;
+		this.d=d;
+	}
+}
